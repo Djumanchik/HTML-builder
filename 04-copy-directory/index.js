d19b9bf4525723f8787dde072e29e688e59c5fc9
Djumanchik <djum83@mail.ru>
2023-05-07 18:57:50 +0300

    feat: done fourth task  7 май 2023 г. 18:57:49

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,21 @@
+const fs = require('fs');
+const path = require('path');
+const dirPath = path.join('/documents', '/coding', '/html-builder', '/04-copy-directory');
 
+fs.mkdir(`${dirPath}/files-copy`, { recursive: true }, (err) => {
+    if (err) {
+      throw err;
+    } else {
+      fs.readdir(
+        `${dirPath}/files`, {withFileTypes: true}, (err, files) => {
+          if (!err) {
+            files.forEach((files) => {
+              fs.copyFile(`${dirPath}/files/${files.name}`, `${dirPath}/files-copy/${files.name}`, () => {
+              });
+            });
+            console.log('Job well done!');
+          }
+        }  
+      );
+    }
+});
