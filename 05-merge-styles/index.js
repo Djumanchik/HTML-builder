9be56e0d9fa96921e77f561a2c8d78bbecceb248
Djumanchik <djum83@mail.ru>
2023-05-07 22:19:31 +0300

    feat: done fifth task  7 май 2023 г. 22:19:30

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const path = require('path');
+const dirPath = path.join('/documents', '/coding', '/html-builder', '/05-merge-styles');
+
+const writeStream = fs.createWriteStream(`${dirPath}/project-dist/bundle.css`);
+
+fs.readdir(`${dirPath}/styles`, {withFileTypes: true}, (err, files) => {
+  if (!err) {
+    files.forEach((files) => {
+      if (files.isFile()) {
+        if ('.css' === path.extname(files.name)) {
+          const readStream = fs.createReadStream(`${dirPath}/styles/${files.name}`, 'utf-8');
+          readStream.pipe(writeStream, {end: false});
+        };
+      };
+    });
+  };
+  console.log('All styles copyed!');
+});
