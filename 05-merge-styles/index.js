c06c5fcc0756b4a81bc7c69d84f3a01c6f8437a0
Kirill Dzhumanov <Djum83@mail.ru>
2023-05-10 16:08:54 +0300

    feat: fix 05 task

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -2,6 +2,10 @@ const fs = require('fs');
 const path = require('path');
 const dirPath = path.join(__dirname);
 
+fs.mkdir(`${dirPath}/project-dist`, { recursive: true }, (err) => {
+  if (err) throw err;
+});
+
 const writeStream = fs.createWriteStream(`${dirPath}/project-dist/bundle.css`);
 
 fs.readdir(`${dirPath}/styles`, {withFileTypes: true}, (err, files) => {
